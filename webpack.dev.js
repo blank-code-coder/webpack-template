efdd6edd62aa25ae8a6c917ce0d1c4ba6ea8cfff
Zain <23135934+blank-code-coder@users.noreply.github.com>
2025-07-29 17:00:36 +0100

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -19,6 +26,18 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
